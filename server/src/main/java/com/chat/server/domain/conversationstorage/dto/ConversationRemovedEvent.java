856ff95f1a8298fe052d6b3f86522275cb2026ac
Maciej Matys <maciej.matys@dynatrace.com>
2022-07-29 21:32:45 +0200

    remove leftovers from previous implementations

--- server/src/main/java/com/chat/server/domain/conversationstorage/dto/ConversationRemovedEvent.java
+++
@@ -1,8 +0,0 @@
-package com.chat.server.domain.conversationstorage.dto;
-
-import java.util.List;
-import java.util.UUID;
-
-public record ConversationRemovedEvent(UUID conversationId,
-                                       List<String> members) {
-}
