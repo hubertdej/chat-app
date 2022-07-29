c55a662cc85bf864ac793b2a084f554853d06ef2
Maciej Matys <maciej.matys@dynatrace.com>
2022-07-29 21:41:05 +0200

    add /chat websocket endpoint

--- server/src/main/java/com/chat/server/infrastructure/websocket/WebSocketConfig.java
+++ server/src/main/java/com/chat/server/infrastructure/websocket/WebSocketConfig.java
@@ -1,24 +1,28 @@
 package com.chat.server.infrastructure.websocket;
 
+import com.chat.server.domain.authentication.AuthenticationFacade;
+import com.chat.server.domain.conversationstorage.ConversationStorageFacade;
+import com.chat.server.domain.messagereceiver.MessageReceiverFacade;
+import com.chat.server.domain.sessionstorage.SessionStorageFacade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import javax.websocket.Session;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Autowired
+    MessageReceiverFacade messageReceiverFacade;
+    @Autowired
+    AuthenticationFacade authenticationFacade;
+    @Autowired
+    SessionStorageFacade sessionStorageFacade;
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/server");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/server-stomp")
-                .withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(messageReceiverFacade, authenticationFacade, sessionStorageFacade), "/chat");
     }
 }
